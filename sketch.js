938584fabc8f5c3213fa43b86f45e69ed27fd069
PranjalbaZala123 <76278708+PranjalbaZala123@users.noreply.github.com>
2021-05-29 12:38:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,12 +16,17 @@ var Bg1;
 var Bg1Img;
 var Bg2;
 var Bg2Img;
-var Ninja;
+var Ninja1;
 var NinjaIdle;
 var NinjaJump;
 var NinjaRun;
+var InvisibleGround;
+var obstaclesGroup;
+var obstacle1;
 
 function preload(){
+
+  // load all images
   BgImg = loadImage("images/Background(1st).jpeg");
   gameNameImg = loadImage("images/GameName.png");
   storyImg = loadImage("images/GameStory.png");
@@ -29,14 +34,18 @@ function preload(){
   NinjaImg = loadImage("images/NinjaImg.png");
   StartImg = loadImage("images/Start.png");
   Bg1Img = loadImage("images/Background2.png");
-  NinjaRun = loadAnimation("images/Ninja11Run.png", "images/Ninja12Run.png", "images/Ninja13Run.png", "images/Ninja14Run.png", "images/Ninja15run.png", "images/Ninja16Run.png");
-  NinjaIdle = loadAnimation("images/Ninja1Id.png", "images/Ninja2Id.png")
 
-
+  obstacle1 = loadImage("images/Obstacle1.png");
   
   
+  NinjaRun = loadAnimation("images/Ninja11Run.png", "images/Ninja12Run.png", "images/Ninja13Run.png", 
+                            "images/Ninja14Run.png", "images/Ninja15run.png", "images/Ninja16Run.png");
 
+  NinjaIdle = loadAnimation("images/Ninja1Id.png", "images/Ninja2Id.png", "images/Ninja3Id.png", "images/Ninja4Id.png");
 
+  NinjaJump = loadAnimation("images/Ninja20Jump.png", "images/Ninja21Jump.png", "images/Ninja22Jump.png",
+                             "images/Ninja23Jump.png", "images/Ninja24Jump.png", "images/Ninja25Jump.png",
+                              "images/Ninja26Jump.png", "images/Ninja27Jump.png" );                        
 
 }
 
@@ -44,6 +53,7 @@ function preload(){
 function setup() {
   createCanvas(displayWidth,displayHeight);
 
+  //create all sprites
   Bg = createSprite( displayWidth/2,displayHeight/2,displayWidth,displayHeight);
   Bg.addImage(BgImg);
   Bg.scale = 1.6;
@@ -66,20 +76,26 @@ function setup() {
 
   Bg1 = createSprite(displayWidth/2,displayHeight/2,displayWidth,displayHeight);
   Bg1.addImage(Bg1Img);
-  Bg1.scale = 2.5;
+  Bg1.scale = 3;
 
-  Bg.visible = false;
-  gameName.visible = false;
-  story.visible = false;
-  rules.visible = false;
-  Ninja.visible = false;
-  Start.visible = false;
-  Bg1.visible = true;
+  InvisibleGround = createSprite(displayWidth/2,displayHeight/2 +280,displayWidth,10);
+
+   Ninja1 = createSprite(displayWidth/2-500, displayHeight/2+200, 10, 10);
+   Ninja1.addAnimation("running", NinjaRun);
+   Ninja1.scale = 2.5;
+   
+   Ninja1.addAnimation("jump", NinjaJump);
+
+
+   //create Obstacle Group
+  obstaclesGroup = createGroup();
+
 
 }
 
 function draw() {
 
+  //Start State
   if(gameState === "start"){
      Bg.visible = true;
      gameName.visible = true;
@@ -88,11 +104,15 @@ function draw() {
      Ninja.visible = true;
      Start.visible = true;
      Bg1.visible = false;
+     Ninja1.visible = false;
+     InvisibleGround.visible = false;
 
      if(mousePressedOver(Start)){
        gameState = "play";
      }
   }
+
+  //Play State
    if(gameState === "play"){
     Bg.visible = false;
     gameName.visible = false;
@@ -102,12 +122,51 @@ function draw() {
     Start.visible = false;
     Bg1.visible = true;
     Bg1.velocityX = -5;
+    Ninja1.visible = true;
+
 
     if(Bg1.x < 0){
        Bg1.x = Bg1.width/2;
     }
 
-   }
+     if(keyWentDown("space")){
+       Ninja1.changeAnimation("jump", NinjaJump);
+       Ninja1.velocityY = -10;
+     }
+
+     if(keyWentUp("space")){
+       Ninja1.changeAnimation("running", NinjaRun);
+     }
+
+
+     Ninja1.collide(InvisibleGround);
+     Ninja1.velocityY = Ninja1.velocityY + 0.5;
+
+
+    //spawn obstacles on the ground
+    spawnObstacles();
+
+
+  }
+
 
   drawSprites();
-}
+  }
+
+
+function spawnObstacles(){
+  if (frameCount % 500 === 0){
+    var obstacle = createSprite(displayWidth,displayHeight/2 + 250,10,10);
+    obstacle.addImage(obstacle1);
+     
+    
+     //assign scale and lifetime to the obstacle           
+     obstacle.scale = 2;
+     obstacle.lifetime = 200;
+     obstacle.velocityX = -15;
+    
+    //add each obstacle to the group
+     obstaclesGroup.add(obstacle);
+  }
+ }
+
